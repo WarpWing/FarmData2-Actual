f3f405d687aa2991cc705820826ce04c4d0e5195
Ty Chermsirivatana <chermsit@dickinson.edu>
2024-12-22 12:56:15 -0500

    feat(dev): added event testing

--- components/PickerBase/PickerBase.events.comp.cy.js
+++ components/PickerBase/PickerBase.events.comp.cy.js
@@ -11,15 +11,6 @@ describe('Test the PickerBase component events', () => {
     cy.saveSessionStorage();
   });
 
-  /**
-   * Does 4 checks on validity computation
-   *
-   * required   empty     test
-   * false      false     1. valid event: Not required, not empty
-   * false      true      2. valid event: Not required, empty
-   * true       false     3. valid event: Required, not empty
-   * true       true      4. valid event: Required, empty
-   */
   it('1. valid event: Not required, not empty', () => {
     const readySpy = cy.spy().as('readySpy');
     const validSpy = cy.spy().as('validSpy');
@@ -262,6 +253,143 @@ describe('Test the PickerBase component events', () => {
         cy.get('[data-cy="picker-options"]').find('input').eq(0).uncheck();
         cy.get('@validSpy').should('have.been.calledTwice');
         cy.get('@validSpy').should('have.been.calledWith', false);
+      });
+  });
+
+  it('Emits update:picked with empty array when All button clears all selections', () => {
+    const readySpy = cy.spy().as('readySpy');
+    const updateSpy = cy.spy().as('updateSpy');
+
+    cy.mount(PickerBase, {
+      props: {
+        onReady: readySpy,
+        'onUpdate:picked': updateSpy,
+        label: 'Picker',
+        options: ['Option 1', 'Option 2', 'Option 3'],
+        picked: ['Option 1', 'Option 2', 'Option 3'],
+        invalidFeedbackText: 'Invalid feedback text.',
+      },
+    });
+
+    cy.get('@readySpy')
+      .should('have.been.calledOnce')
+      .then(() => {
+        cy.get('[data-cy="picker-all-button"]').click();
+        cy.get('@updateSpy').should('have.been.calledOnce');
+        cy.get('@updateSpy').should('have.been.calledWith', []);
+      });
+  });
+
+  it('Emits update:picked with all options when All button selects all', () => {
+    const readySpy = cy.spy().as('readySpy');
+    const updateSpy = cy.spy().as('updateSpy');
+
+    cy.mount(PickerBase, {
+      props: {
+        onReady: readySpy,
+        'onUpdate:picked': updateSpy,
+        label: 'Picker',
+        options: ['Option 1', 'Option 2', 'Option 3'],
+        invalidFeedbackText: 'Invalid feedback text.',
+      },
+    });
+
+    cy.get('@readySpy')
+      .should('have.been.calledOnce')
+      .then(() => {
+        cy.get('[data-cy="picker-all-button"]').click();
+        cy.get('@updateSpy').should('have.been.calledOnce');
+        cy.get('@updateSpy').should('have.been.calledWith', [
+          'Option 1',
+          'Option 2',
+          'Option 3',
+        ]);
+      });
+  });
+
+  it('Emits valid event when All button makes selection valid', () => {
+    const readySpy = cy.spy().as('readySpy');
+    const validSpy = cy.spy().as('validSpy');
+
+    cy.mount(PickerBase, {
+      props: {
+        onReady: readySpy,
+        onValid: validSpy,
+        required: true,
+        label: 'Picker',
+        options: ['Option 1', 'Option 2', 'Option 3'],
+        invalidFeedbackText: 'Invalid feedback text.',
+      },
+    });
+
+    cy.get('@readySpy')
+      .should('have.been.calledOnce')
+      .then(() => {
+        cy.get('@validSpy').should('have.been.calledOnce');
+        cy.get('@validSpy').should('have.been.calledWith', false);
+        
+        cy.get('[data-cy="picker-all-button"]').click();
+        cy.get('@validSpy').should('have.been.calledTwice');
+        cy.get('@validSpy').should('have.been.calledWith', true);
+      });
+  });
+
+  it('Emits valid event when All button makes selection invalid', () => {
+    const readySpy = cy.spy().as('readySpy');
+    const validSpy = cy.spy().as('validSpy');
+
+    cy.mount(PickerBase, {
+      props: {
+        onReady: readySpy,
+        onValid: validSpy,
+        required: true,
+        label: 'Picker',
+        options: ['Option 1', 'Option 2', 'Option 3'],
+        picked: ['Option 1', 'Option 2', 'Option 3'],
+        invalidFeedbackText: 'Invalid feedback text.',
+      },
+    });
+
+    cy.get('@readySpy')
+      .should('have.been.calledOnce')
+      .then(() => {
+        cy.get('@validSpy').should('have.been.calledOnce');
+        cy.get('@validSpy').should('have.been.calledWith', true);
+        
+        cy.get('[data-cy="picker-all-button"]').click();
+        cy.get('@validSpy').should('have.been.calledTwice');
+        cy.get('@validSpy').should('have.been.calledWith', false);
+      });
+  });
+
+  it('Emits update:picked when All button toggles between states', () => {
+    const readySpy = cy.spy().as('readySpy');
+    const updateSpy = cy.spy().as('updateSpy');
+
+    cy.mount(PickerBase, {
+      props: {
+        onReady: readySpy,
+        'onUpdate:picked': updateSpy,
+        label: 'Picker',
+        options: ['Option 1', 'Option 2', 'Option 3'],
+        invalidFeedbackText: 'Invalid feedback text.',
+      },
+    });
+
+    cy.get('@readySpy')
+      .should('have.been.calledOnce')
+      .then(() => {
+        cy.get('[data-cy="picker-all-button"]').click();
+        cy.get('@updateSpy').should('have.been.calledOnce');
+        cy.get('@updateSpy').should('have.been.calledWith', [
+          'Option 1',
+          'Option 2',
+          'Option 3',
+        ]);
+
+        cy.get('[data-cy="picker-all-button"]').click();
+        cy.get('@updateSpy').should('have.been.calledTwice');
+        cy.get('@updateSpy').should('have.been.calledWith', []);
       });
   });
 });
